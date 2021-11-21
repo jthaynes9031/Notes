416771cc3c13d858520e6f71a3a5b3c1949bd898
Jonathan Haynes <55987768+jthaynes9031@users.noreply.github.com>
2021-11-20 22:32:14 -0500

    Add files via upload

--- Notes/day4.py
+++ Notes/day4.py
@@ -1,1 +1,23 @@
 #Randomisation
+import random
+
+random_number = random.randint(1,50)
+print (random_number)
+
+#prints floats
+#random_f = random.random(0,1)
+#print(random_f)
+
+#you can have different modules and by doing so is making another file and importing it to anoter
+#make new file 
+#import file in main.py
+
+#heads or tails
+heads_or_tails = int(input("type '1' for heads type '2' for 'tails'"))
+h_f = random.randint(1,2)
+if(h_f == heads_or_tails):
+    print(h_f)
+    print(f"you chose {heads_or_tails} you win")
+else:
+    print(h_f)
+    print(f"you chose {heads_or_tails} you lose ")
